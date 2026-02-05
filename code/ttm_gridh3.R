ec73d2291dd7f47bcc1b74fb899b051209722f9e
gmatosferreira <goncaloafmatos@tecnico.ulisboa.pt>
2026-02-05 11:41:04 +0000

    #6 ttm computation refactored to for loop to simplify running process

--- code/ttm_gridh3.R
+++ code/ttm_gridh3.R
@@ -1,308 +1,90 @@
 # run ttm to all cell combinations and all modes
 
 library(tidyverse)
-library(sf)
-options(java.parameters = '-Xmx30G') # RAM to 8GB
-library(r5r)
 library(interp)
 
-# load network
-r5r_network = build_network(data_path = "/data/IMPT/geo/r5r/") # already existing network model
+# load network, variable r5r_network using data_load.R
+r5r_network
 
 # set r5r parameters
+# root_folder = "/data/IMPT"
+root_folder = "data" # Set to "/data/IMPT/ when running at server.ushift.pt, or "data" when running locally
 points = points_h3
+grid_name = "h3_res8"
 nrow(points) # 3686 - this is the res 8 h3 grid
 mode_egress = "WALK"
 max_walk_time = 15 # 20?
 max_lts = 3 # for bike
 
 # parameters to vary
-departure_datetime_HP = as.POSIXct("04-02-2026 08:00:00", format = "%d-%m-%Y %H:%M:%S")
-departure_datetime_FHP = as.POSIXct("08-02-2026 20:00:00", format = "%d-%m-%Y %H:%M:%S")
-departure_datetime_night = as.POSIXct("04-02-2026 03:00:00", format = "%d-%m-%Y %H:%M:%S")
+departure_datetime_HP = as.POSIXct("04-02-2026 08:00:00", format = "%d-%m-%Y %H:%M:%S") 
+departure_datetime_FHP = as.POSIXct("08-02-2026 20:00:00", format = "%d-%m-%Y %H:%M:%S") 
+departure_datetime_night = as.POSIXct("04-02-2026 03:00:00", format = "%d-%m-%Y %H:%M:%S") 
 max_trip_duration_120 = 120 # 2 hours
 max_trip_duration_60 = 60 # 1 hours
 max_rides_2 = 2 # 1 transfers
 max_rides_3 = 3 # 2 transfers
 
-
-
-# Car ---------------------------------------------------------------------
-
-# 1 hour
-ttm_car_1h = travel_time_matrix(r5r_network,
-                             origins = points,
-                             destinations = points,
-                             mode = "CAR",
-                             departure_datetime = departure_datetime_HP,
-                             max_trip_duration = max_trip_duration_60,
-                             verbose = FALSE)
-
-nrow(ttm_car_1h)
-saveRDS(ttm_car_1h, "/data/IMPT/ttm/ttm_h3_res8/ttm_car_1h.rds")
-
-
-# 2 hour
-ttm_car_2h = travel_time_matrix(r5r_network,
-                                origins = points,
-                                destinations = points,
-                                mode = "CAR",
-                                departure_datetime = departure_datetime_HP,
-                                max_trip_duration = max_trip_duration_120,
-                                verbose = FALSE)
-
-nrow(ttm_car_2h)
-saveRDS(ttm_car_2h, "/data/IMPT/ttm/ttm_h3_res8/ttm_car_2h.rds")
-
-
-# Bike --------------------------------------------------------------------
-
-# 1 hour
-ttm_bike_1h = travel_time_matrix(r5r_network,
-                             origins = points,
-                             destinations = points,
-                             mode = "BICYCLE",
-                             departure_datetime = departure_datetime_HP,
-                             max_trip_duration = max_trip_duration_60,
-                             max_lts = max_lts,
-                             verbose = FALSE)
-
-nrow(ttm_bike_1h)
-saveRDS(ttm_bike_1h, "/data/IMPT/ttm/ttm_h3_res8/ttm_bike_1h.rds")
-
-# 2 hour
-ttm_bike_2h = travel_time_matrix(r5r_network,
-                                 origins = points,
-                                 destinations = points,
-                                 mode = "BICYCLE",
-                                 departure_datetime = departure_datetime_HP,
-                                 max_trip_duration = max_trip_duration_120,
-                                 max_lts = max_lts,
-                                 verbose = FALSE)
-
-nrow(ttm_bike_2h)
-saveRDS(ttm_bike_2h, "/data/IMPT/ttm/ttm_h3_res8/ttm_bike_2h.rds")
-
-
-
-# Walk --------------------------------------------------------------------
-
-# 1 hour
-ttm_walk_1h = travel_time_matrix(r5r_network,
-                              origins = points,
-                              destinations = points,
-                              mode = "WALK",
-                              departure_datetime = departure_datetime_HP,
-                              max_trip_duration = max_trip_duration_60,
-                              verbose = FALSE)
-
-nrow(ttm_walk_1h)
-saveRDS(ttm_walk_1h, "/data/IMPT/ttm/ttm_h3_res8/ttm_walk_1h.rds")
-
-# 2 hour
-ttm_walk_2h = travel_time_matrix(r5r_network,
-                                 origins = points,
-                                 destinations = points,
-                                 mode = "WALK",
-                                 departure_datetime = departure_datetime_HP,
-                                 max_trip_duration = max_trip_duration_120,
-                                 verbose = FALSE)
-
-nrow(ttm_walk_2h)
-saveRDS(ttm_walk_2h, "/data/IMPT/ttm/ttm_h3_res8/ttm_walk_2h.rds")
-
-
-
-# PTransit ----------------------------------------------------------------
-
-## 1 hour
-# 1 hour, 1 transfer, HP
-ttm_pt_1h_1t_HP = travel_time_matrix(r5r_network,
-                                 origins = points,
-                                 destinations = points,
-                                 mode = "TRANSIT",
-                                 departure_datetime = departure_datetime_HP,
-                                 max_trip_duration = max_trip_duration_60,
-                                 max_rides = max_rides_2,
-                                 egress_mode = mode_egress,
-                                 max_walk_time = max_walk_time,
-                                 verbose = FALSE)
-
-nrow(ttm_pt_1h_1t_HP)
-saveRDS(ttm_pt_1h_1t_HP, "/data/IMPT/ttm/ttm_h3_res8/ttm_pt_1h_1t_HP.rds")
-
-# 1 hour, 2 transfers, HP
-ttm_pt_1h_2t_HP = travel_time_matrix(r5r_network,
-                                 origins = points,
-                                 destinations = points,
-                                 mode = "TRANSIT",
-                                 departure_datetime = departure_datetime_HP,
-                                 max_trip_duration = max_trip_duration_60,
-                                 max_rides = max_rides_3,
-                                 egress_mode = mode_egress,
-                                 max_walk_time = max_walk_time,
-                                 verbose = FALSE)
-
-nrow(ttm_pt_1h_2t_HP)
-saveRDS(ttm_pt_1h_2t_HP, "/data/IMPT/ttm/ttm_h3_res8/ttm_pt_1h_2t_HP.rds")
-
-
-# 1 hour, 1 transfer, FHP
-ttm_pt_1h_1t_FHP = travel_time_matrix(r5r_network,
-                                 origins = points,
-                                 destinations = points,
-                                 mode = "TRANSIT",
-                                 departure_datetime = departure_datetime_FHP,
-                                 max_trip_duration = max_trip_duration_60,
-                                 max_rides = max_rides_2,
-                                 egress_mode = mode_egress,
-                                 max_walk_time = max_walk_time,
-                                 verbose = FALSE)
-
-nrow(ttm_pt_1h_1t_FHP)
-saveRDS(ttm_pt_1h_1t_FHP, "/data/IMPT/ttm/ttm_h3_res8/ttm_pt_1h_1t_FHP.rds")
-
-# 1 hour, 2 transfers, FHP
-ttm_pt_1h_2t_FHP = travel_time_matrix(r5r_network,
-                                 origins = points,
-                                 destinations = points,
-                                 mode = "TRANSIT",
-                                 departure_datetime = departure_datetime_FHP,
-                                 max_trip_duration = max_trip_duration_60,
-                                 max_rides = max_rides_3,
-                                 egress_mode = mode_egress,
-                                 max_walk_time = max_walk_time,
-                                 verbose = FALSE)
-
-nrow(ttm_pt_1h_2t_FHP)
-saveRDS(ttm_pt_1h_2t_FHP, "/data/IMPT/ttm/ttm_h3_res8/ttm_pt_1h_2t_FHP.rds")
-
-# 1 hour, 1 transfer, night
-ttm_pt_1h_1t_night = travel_time_matrix(r5r_network,
-                                          origins = points,
-                                          destinations = points,
-                                          mode = "TRANSIT",
-                                          departure_datetime = departure_datetime_night,
-                                          max_trip_duration = max_trip_duration_60,
-                                          max_rides = max_rides_2,
-                                          egress_mode = mode_egress,
-                                          max_walk_time = max_walk_time,
-                                          verbose = FALSE)
-
-nrow(ttm_pt_1h_1t_night)
-saveRDS(ttm_pt_1h_1t_night, "/data/IMPT/ttm/ttm_h3_res8/ttm_pt_1h_1t_night.rds")
-
-# 1 hour, 2 transfers, night
-ttm_pt_1h_2t_night = travel_time_matrix(r5r_network,
-                                          origins = points,
-                                          destinations = points,
-                                          mode = "TRANSIT",
-                                          departure_datetime = departure_datetime_night,
-                                          max_trip_duration = max_trip_duration_60,
-                                          max_rides = max_rides_3,
-                                          egress_mode = mode_egress,
-                                          max_walk_time = max_walk_time,
-                                          verbose = FALSE)
-
-nrow(ttm_pt_1h_2t_night)
-saveRDS(ttm_pt_1h_2t_night, "/data/IMPT/ttm/ttm_h3_res8/ttm_pt_1h_2t_night.rds")
-
-## 2 hour
-# 2 hour, 1 transfer, HP
-ttm_pt_2h_1t_HP = travel_time_matrix(r5r_network,
-                                        origins = points,
-                                        destinations = points,
-                                        mode = "TRANSIT",
-                                        departure_datetime = departure_datetime_HP,
-                                        max_trip_duration = max_trip_duration_120,
-                                        max_rides = max_rides_2,
-                                        egress_mode = mode_egress,
-                                        max_walk_time = max_walk_time,
-                                        verbose = FALSE)
-
-nrow(ttm_pt_2h_1t_HP)
-saveRDS(ttm_pt_2h_1t_HP, "/data/IMPT/ttm/ttm_h3_res8/ttm_pt_2h_1t_HP.rds")
-
-# 2 hour, 2 transfers, HP
-ttm_pt_2h_2t_HP = travel_time_matrix(r5r_network,
-                                        origins = points,
-                                        destinations = points,
-                                        mode = "TRANSIT",
-                                        departure_datetime = departure_datetime_HP,
-                                        max_trip_duration = max_trip_duration_120,
-                                        max_rides = max_rides_3,
-                                        egress_mode = mode_egress,
-                                        max_walk_time = max_walk_time,
-                                        verbose = FALSE)
-
-nrow(ttm_pt_2h_2t_HP)
-saveRDS(ttm_pt_2h_2t_HP, "/data/IMPT/ttm/ttm_h3_res8/ttm_pt_2h_2t_HP.rds")
-
-
-# 2 hour, 1 transfer, FHP
-ttm_pt_2h_1t_FHP = travel_time_matrix(r5r_network,
-                                          origins = points,
-                                          destinations = points,
-                                          mode = "TRANSIT",
-                                          departure_datetime = departure_datetime_FHP,
-                                          max_trip_duration = max_trip_duration_120,
-                                          max_rides = max_rides_2,
-                                          egress_mode = mode_egress,
-                                          max_walk_time = max_walk_time,
-                                          verbose = FALSE)
-
-nrow(ttm_pt_2h_1t_FHP)
-saveRDS(ttm_pt_2h_1t_FHP, "/data/IMPT/ttm/ttm_h3_res8/ttm_pt_2h_1t_FHP.rds")
-
-# 2 hour, 2 transfers, FHP
-ttm_pt_2h_2t_FHP = travel_time_matrix(r5r_network,
-                                          origins = points,
-                                          destinations = points,
-                                          mode = "TRANSIT",
-                                          departure_datetime = departure_datetime_FHP,
-                                          max_trip_duration = max_trip_duration_120,
-                                          max_rides = max_rides_3,
-                                          egress_mode = mode_egress,
-                                          max_walk_time = max_walk_time,
-                                          verbose = FALSE)
-
-nrow(ttm_pt_2h_2t_FHP)
-saveRDS(ttm_pt_2h_2t_FHP, "/data/IMPT/ttm/ttm_h3_res8/ttm_pt_2h_2t_FHP.rds")
-
-# 2 hour, 1 transfer, night
-ttm_pt_2h_1t_night = travel_time_matrix(r5r_network,
-                                            origins = points,
-                                            destinations = points,
-                                            mode = "TRANSIT",
-                                            departure_datetime = departure_datetime_night,
-                                            max_trip_duration = max_trip_duration_120,
-                                            max_rides = max_rides_2,
-                                            egress_mode = mode_egress,
-                                            max_walk_time = max_walk_time,
-                                            verbose = FALSE)
-
-nrow(ttm_pt_2h_1t_night)
-saveRDS(ttm_pt_2h_1t_night, "/data/IMPT/ttm/ttm_h3_res8/ttm_pt_2h_1t_night.rds")
-
-# 2 hour, 2 transfers, night
-ttm_pt_2h_2t_night = travel_time_matrix(r5r_network,
-                                            origins = points,
-                                            destinations = points,
-                                            mode = "TRANSIT",
-                                            departure_datetime = departure_datetime_night,
-                                            max_trip_duration = max_trip_duration_120,
-                                            max_rides = max_rides_3,
-                                            egress_mode = mode_egress,
-                                            max_walk_time = max_walk_time,
-                                            verbose = FALSE)
-
-nrow(ttm_pt_2h_2t_night)
-saveRDS(ttm_pt_2h_2t_night, "/data/IMPT/ttm/ttm_h3_res8/ttm_pt_2h_2t_night.rds")
-
-
-
+# main()
+folder_name = sprintf("%s/ttm/ttm_%s", root_folder, tolower(grid_name))
+if(!dir.exists(folder_name)) {
+  dir.create(folder_name, recursive = TRUE)
+}
+  
+for (mode in c("TRANSIT")) { # c("CAR", "BICYCLE", "WALK", "TRANSIT")
+  for (max_trip_duration in c(60, 120)) {
+    message(paste("Running travel time matrix for mode:", mode, "max trip duration:", max_trip_duration))
+    
+    # Static parameters
+    args = list()
+    args$r5r_network = r5r_network
+    args$origins = points
+    args$destinations = points
+    
+    # Varying parameters
+    args$mode = mode
+    args$max_trip_duration = max_trip_duration
+    
+    # > Transit has multiple departure times
+    departures = c(departure_datetime_HP)
+    if (mode == "TRANSIT") {
+      departures = c(departure_datetime_HP, departure_datetime_FHP, departure_datetime_night)
+    }
+    for (departure_datetime in departures) {
+      departure_datetime = as.POSIXct(departure_datetime, origin_tz = "Europe/Lisbon")
+      args$departure_datetime = departure_datetime
+      args$verbose = FALSE
+      if (mode == "BICYCLE") {
+        args$max_lts = max_lts
+      }
+      # > Transit has multiple max rides
+      max_rides = c(NA)
+      if (mode == "TRANSIT") {
+        args$mode_egress = mode_egress
+        args$max_walk_time = max_walk_time
+        max_rides = c(max_rides_2, max_rides_3)
+      }
+      
+      for (mr in max_rides) {
+        if (!is.na(mr)) {
+          args$max_rides = mr
+        }
+        
+        filename = sprintf("%s/ttm_%s_%dmin_%s", folder_name, tolower(mode), max_trip_duration, strftime(departure_datetime, "%Y%m%d%H%M", tz = "Europe/Lisbon"))
+        if (!is.na(mr)) {
+          filename = sprintf("%s_%dtransfers.rds", filename, mr-1)
+        } else {
+          filename = sprintf("%s.rds", filename)
+        }
+        ttm = do.call(travel_time_matrix, args)
+        message("Saving to ", filename)
+        saveRDS(ttm, filename)
+        message("Done :)")
+      }
+    }
+  }
+}
 
 # stop r5r ----------------------------------------------------------------
-
 r5r::stop_r5(r5r_network)
 rJava::.jgc(R.gc = TRUE)
