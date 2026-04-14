687db357d59ff8ffee6ddb715664d36c8389d936
temospena <temospena@gmail.com>
2026-04-14 16:20:06 +0100

    correct name cos layer #20

--- code/02_census_grid_with_cos.R
+++ code/02_census_grid_with_cos.R
@@ -190,4 +190,5 @@ summary(grid_final$population_density) # sounds good
 st_write(grid_final, IMPT_URL("/landuse/grid_with_cos.gpkg"), delete_dsn = TRUE)
 write.csv(grid_final |> st_drop_geometry(), IMPT_URL("/landuse/grid_with_cos.csv"), row.names = FALSE)
 
-st_write(cos_clean, IMPT_URL("/geo/grid_with_cos.geojson"), delete_dsn = TRUE)
+st_write(cos_clean, IMPT_URL("/geo/cos_builtarea.geojson"), delete_dsn = TRUE)
+
