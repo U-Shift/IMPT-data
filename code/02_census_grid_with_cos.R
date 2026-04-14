7238a4d758c11fe22eac8b45a6e6483385038058
gmatosferreira <goncaloafmatos@tecnico.ulisboa.pt>
2026-04-14 14:34:44 +0100

    #20 census grid with cos calculations improved to compute pp_hh_avg with fallbacks when no population (or low household number)

--- code/02_census_grid_with_cos.R
+++ code/02_census_grid_with_cos.R
@@ -15,17 +15,17 @@ library(mapview)
 # cos_raw = st_read("/data/IMPT/original/COS2023v1-S2.gpkg")
 # unique(cos_raw$COS23_n4_L)
 # unique(cos_raw$COS23_n4_C)
-# 
+#
 # cos_residential = cos_raw |> filter(COS23_n4_L %in% c(
 #   "Áreas edificadas residenciais contínuas predominantemente verticais",
 #   "Áreas edificadas residenciais contínuas predominantemente horizontais",
 #   "Áreas edificadas residenciais descontínuas",
 #   "Áreas edificadas residenciais descontínuas esparsas"
 # ))
-# 
+#
 # # mapview(cos_residential)
-# 
-# 
+#
+#
 # # filter aml
 # clip_bbox = st_read("/data/IMPT/geo/clipping_boundary.geojson")
 # cos_residential_aml = cos_residential |> st_transform(crs = st_crs(clip_bbox))
@@ -38,23 +38,23 @@ library(mapview)
 
 # with grid ---------------------------------------------------------------
 
-cos_residential_aml = st_read("/data/IMPT/geo/cos_residential_aml.gpkg")
-grid = st_read("/data/IMPT/geo/grelha_h3_r8.gpkg")
-census_bgri = st_read("/data/IMPT/original/BGRI21_170.gpkg")
+cos_residential_aml <- st_read(IMPT_URL("/geo/cos_residential_aml.gpkg"))
+grid <- st_read(IMPT_URL("/geo/grelha_h3_r8.gpkg"))
+census_bgri <- st_read(IMPT_URL("/original/BGRI21_170.gpkg"))
 
-mapview(cos_residential_aml, zcol = "COS23_n4_L") + 
+mapview(cos_residential_aml, zcol = "COS23_n4_L") +
   mapview(grid, alpha = 0.3)
-mapview(cos_residential_aml, zcol = "COS23_n4_L") + 
+mapview(cos_residential_aml, zcol = "COS23_n4_L") +
   mapview(census_bgri, alpha = 0.2)
 
 
 #### test with densymetric grid
 # --- STEP 1: CRS & CLEANING ---
 # Use EPSG:3763 for Portugal (Meters)
-target_crs <- 3763 
+target_crs <- 3763
 
 census_bgri <- st_transform(census_bgri, target_crs) %>% st_make_valid()
-grid_meters        <- st_transform(grid, target_crs)
+grid_meters <- st_transform(grid, target_crs)
 cos_habitable <- cos_residential_aml %>%
   rename(classification = COS23_n4_L) %>%
   mutate(weight = case_when(
@@ -69,7 +69,7 @@ cos_habitable <- cos_residential_aml %>%
 cos_clean <- cos_habitable %>%
   group_by(classification) %>%
   summarise(weight = first(weight)) %>%
-  st_simplify(dTolerance = 1) %>% 
+  st_simplify(dTolerance = 1) %>%
   st_make_valid()
 
 # --- STEP 3: SPATIAL JOIN LOGIC ---
@@ -80,8 +80,8 @@ grid_active <- grid_meters[st_intersects(grid_meters, census_bgri, sparse = FALS
 grid_weighted <- st_intersection(grid_active, cos_clean)
 
 # Simplify Census to Polygons for speed
-census_simple <- census_bgri %>% 
-  st_cast("POLYGON") %>% 
+census_simple <- census_bgri %>%
+  st_cast("POLYGON") %>%
   st_make_valid()
 
 # Final Intersection (The "Chopping")
@@ -108,13 +108,13 @@ grid_populated <- final_intersection %>%
     total_pop = sum(fragment_pop, na.rm = TRUE),
     weighted_urban_index = mean(weight, na.rm = TRUE),
     .groups = "drop"
-  ) 
+  )
 # the results are still fragments, not hex cells
 
 
 # Check
 sum(census_bgri$N_INDIVIDUOS, na.rm = TRUE) # 2870208
-sum(grid_populated$total_pop, na.rm = TRUE) # 2853546 
+sum(grid_populated$total_pop, na.rm = TRUE) # 2853546
 # difference = 16662 # better than the census centroids!! (was 19.562)
 # 0.58 %
 
@@ -122,23 +122,23 @@ sum(grid_populated$total_pop, na.rm = TRUE) # 2853546
 h3_pop_table <- final_intersection %>%
   st_drop_geometry() %>%
   mutate(frag_weight_score = frag_area * weight) %>%
-  group_by(BGRI2021) %>% 
+  group_by(BGRI2021) %>%
   mutate(total_bgri_weight_score = sum(frag_weight_score)) %>%
   ungroup() %>%
-  filter(total_bgri_weight_score > 0) %>% 
+  filter(total_bgri_weight_score > 0) %>%
   mutate(
     pop_fraction = frag_weight_score / total_bgri_weight_score,
-    fragment_buildings           = N_EDIFICIOS_CLASSICOS * pop_fraction,
-    fragment_buildings_pre1945   = N_EDIFICIOS_CONSTR_ANTES_1945 * pop_fraction,
+    fragment_buildings = N_EDIFICIOS_CLASSICOS * pop_fraction,
+    fragment_buildings_pre1945 = N_EDIFICIOS_CONSTR_ANTES_1945 * pop_fraction,
     fragment_population = N_INDIVIDUOS * pop_fraction,
     fragment_households = N_NUCLEOS_FAMILIARES * pop_fraction, # households
-    fragment_youth               = N_INDIVIDUOS_0_14 * pop_fraction,
-    fragment_elderly             = N_INDIVIDUOS_65_OU_MAIS * pop_fraction,
+    fragment_youth = N_INDIVIDUOS_0_14 * pop_fraction,
+    fragment_elderly = N_INDIVIDUOS_65_OU_MAIS * pop_fraction,
     fragment_adults = (N_INDIVIDUOS_15_24 + N_INDIVIDUOS_25_64) * pop_fraction,
-    fragment_women               = N_INDIVIDUOS_M * pop_fraction
+    fragment_women = N_INDIVIDUOS_M * pop_fraction
   ) %>%
   # NOW group by the H3 index to combine all fragments in that cell
-  group_by(id) %>% 
+  group_by(id) %>%
   summarise(
     buildings = sum(fragment_buildings, na.rm = TRUE),
     buildings_pre1945 = sum(fragment_buildings_pre1945, na.rm = TRUE),
@@ -154,9 +154,10 @@ h3_pop_table <- final_intersection %>%
 # 2. Join back to the original Grid to get full Hexagon geometries
 grid_final <- grid %>%
   left_join(h3_pop_table, by = "id") %>%
+  mutate(households = if_else(households > 0 & households < 1, 1, households)) %>% # Prevent 0.5 households
   mutate(
     # Fill NAs with 0 first so the math doesn't break
-    across(c(population, youth, elderly, women, buildings, buildings_pre1945), ~replace_na(.x, 0)),
+    across(c(population, youth, elderly, women, buildings, buildings_pre1945), ~ replace_na(.x, 0)),
     area_km2 = as.numeric(st_area(.)) / 1000000,
     population_density = population / area_km2,
     youth_ratio = if_else(population > 0, round(youth / population * 100, 2), 0),
@@ -164,21 +165,23 @@ grid_final <- grid %>%
     youth_dependency = if_else(population > 0, round(youth / adults * 100, 2), 0),
     elderly_dependency = if_else(population > 0, round(elderly / adults * 100, 2), 0),
     women_percentage = if_else(population > 0, round(women / population * 100, 2), 0),
+    pp_hh_avg = ifelse(population > 0, ifelse(households > 0, round(population / households, 2), population), 0),
     buildings_pre1945_percentage = if_else(buildings > 0, round(buildings_pre1945 / buildings * 100, 2), 0)
   ) %>%
   mutate( # solve to avoud inf and nan
-    youth_dependency = if_else(is.nan(youth_dependency), 0, youth_dependency), 
-    elderly_dependency = if_else(is.infinite(elderly_dependency), 100, elderly_dependency)) |> 
+    youth_dependency = if_else(is.nan(youth_dependency), 0, youth_dependency),
+    elderly_dependency = if_else(is.infinite(elderly_dependency), 100, elderly_dependency)
+  ) |>
   mutate(across(c(buildings, buildings_pre1945, population, youth, elderly, adults, women), ceiling)) %>%
   st_transform(4326)
 
-sum(grid_final$population) #2854660
+sum(grid_final$population) # 2854660
 # difference = 15548 # even better
-mapview(grid_final |> filter(population > 0), zcol="population")
-mapview(grid_final |> filter(population > 0), zcol="elderly_dependency")
+mapview(grid_final |> filter(population > 0), zcol = "population")
+mapview(grid_final |> filter(population > 0), zcol = "elderly_dependency")
 
 summary(grid_final$population_density) # sounds good
 
 # export
-st_write(grid_final, "/data/IMPT/landuse/grid_with_cos.gpkg")
-
+st_write(grid_final, IMPT_URL("/landuse/grid_with_cos.gpkg"), delete_dsn = TRUE)
+write.csv(grid_final |> st_drop_geometry(), IMPT_URL("/landuse/grid_with_cos.csv"), row.names = FALSE)
