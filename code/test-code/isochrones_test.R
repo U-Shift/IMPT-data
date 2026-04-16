c2286b3e5edac5ea6bea49f220583387df9fe7f9
temospena <temospena@gmail.com>
2026-04-16 16:49:42 +0100

    using r5r for isochrones by stop type #7

--- code/test-code/isochrones_test.R
+++ code/test-code/isochrones_test.R
@@ -1,91 +1,70 @@
 #testing isochrones for PT stops
+# Alternative way of isochrones to PT stops, with time and type of pt
 
-
-options(openrouteservice.url = "https://server.ushift.pt/ors/")
-# Set credentials
-Sys.setenv(ORS_API_KEY = "AMwi5LU1NCe1ERyQuUHTe2pulOFKcdq0")
-# Sys.unsetenv("ORS_API_KEY")
-# Validate
-getOption("openrouteservice.url")
-#> [1] "https://server.ushift.pt/ors/"
-Sys.getenv("ORS_API_KEY")
-#> [1] "AMwi5LU1NCe1ERyQuUHTe2pulOFKcdq0"
-
-# Start by getting all PT stops in AML
-# Attention! Must be run at ushift@alfa, or addapted to use local (make sure to upload to server after)
-gtfs_paths <- list.files("/data/gtfs/processed", pattern = "\\.zip$", full.names = TRUE)
-all_stops <- list()
-for (i in gtfs_paths) {
-  gtfs_original <- tidytransit::read_gtfs(i)
-  message("Running for ", gtfs_original$agency$agency_name[1])
-  gtfs <- tidytransit::filter_feed_by_date(gtfs_original, "2026-02-04")
-  summary(gtfs)
-  stops_sf <- tidytransit::stops_as_sf(gtfs$stops)
-  all_stops[[i]] <- stops_sf
-}
-# mapview(all_stops, legend=FALSE)
-
-# Create euclidean buffers for all PT stops
-all_stops_combined <- bind_rows(all_stops) |>
-  st_as_sf() |>
-  st_filter(limit_bbox)
-# mapview(all_stops_combined, legend=FALSE)
-all_stops_combined <- all_stops_combined |> select(stop_id, stop_code, stop_name, geometry)
-stops_buffers <- st_buffer(
-  # Convert to 3857 crs
-  all_stops_combined |> st_transform(3857),
-  dist = 500
-) # 500m euclidean buffer around stops
-
-# Create service area buffers for each type of PT stops
-train_ferry_stations = bind_rows(all_stops$`/data/IMPT/gtfs/processed/gtfs_comboios_de_portugal.zip`,all_stops$`/data/IMPT/gtfs/processed/gtfs_fertagus.zip`,all_stops$`/data/IMPT/gtfs/processed/gtfs_transtejo_soflusa.zip`) |> st_as_sf() |> st_filter(limit_bbox)
-train_ferry_stations <- train_ferry_stations |> select(stop_id, stop_code, stop_name, geometry) |> st_transform(4326)
-train_ferry_coordinates <- st_coordinates(train_ferry_stations) |> as.data.frame() |> select(X, Y) |> rename(lon=X, lat=Y) |> mutate(id=train_ferry_stations$stop_id) |> select(id, lon, lat)
-# stop_service_areas <- ors_isochrones(
-#   locations = train_ferry_coordinates,
-#   profile = "foot-walking",
-#   range = 15,
-#   range_type = "time"
-# )
-metro_stations = bind_rows(all_stops$`/data/IMPT/gtfs/processed/gtfs_metropolitano_de_lisboa.zip`,all_stops$`/data/IMPT/gtfs/processed/gtfs_metro_transportes_do_sul.zip`) |> st_as_sf() |> st_filter(limit_bbox)
-metro_stations <- metro_stations |> select(stop_id, stop_code, stop_name, geometry) |> st_transform(4326)
-metro_coordinates <- st_coordinates(metro_stations) |> as.data.frame() |> select(X, Y) |> rename(lon=X, lat=Y)
-# stop_service_areas <- ors_isochrones(
-#   locations = metro_coordinates,
-#   profile = "foot-walking",
-#   range = 5,
-#   range_type = "time"
-# )
-bus_stations = bind_rows(all_stops$`/data/IMPT/gtfs/processed/gtfs_carris_metropolitana.zip`,all_stops$`/data/IMPT/gtfs/processed/gtfs_carris_municipal.zip`,all_stops$`/data/IMPT/gtfs/processed/gtfs_mobicascais.zip`,all_stops$`/data/IMPT/gtfs/processed/gtfs_transportes_colectivos_do_barreiro.zip`) |> st_as_sf() |> st_filter(limit_bbox)
-bus_stations <- bus_stations |> select(stop_id, stop_code, stop_name, geometry) |> st_transform(4326)
-bus_coordinates <- st_coordinates(bus_stations) |> as.data.frame() |> select(X, Y) |> rename(lon=X, lat=Y)
-# stop_service_areas <- ors_isochrones(
-#   locations = bus_coordinates,
-#   profile = "foot-walking",
-#   range = 5,
-#   range_type = "time"
-# )
-
-### Get isochrones for train/ferry stations using r5r (PRELIMINARY - NOT COMPLETE)
-# system.file returns the directory with example data inside the r5r package
+library(stringr)
+library(dplyr)
+options(java.parameters = "-Xmx32G") # RAM to 16GB
+library(r5r)
 # set data path to directory containing your own data if not running this example
 r5r_network <- r5r::build_network("/data/IMPT/geo/r5r/")
-# data_path <- "/data/IMPT/geo/r5r"
-# r5r_network <- build_network(data_path)
-# isochrone intervals
-time_interval <- 15
-# routing inputs
-mode <- "WALK"
-departure_datetime <- as.POSIXct("13-05-2019 14:00:00", format = "%d-%m-%Y %H:%M:%S")
-# calculate travel time matrix
+limit = impt_read("/geo/municipios_union_2024.geojson") |> sf::st_make_valid() # aml limit
+
+# get stops being used
+transit_network = transit_network_to_sf(r5r_network)
+stops_transit = transit_network$stops
+stops_transit = stops_transit |> st_filter(limit) |> select(-linked_to_street) # keep only stops in aml
+
+# classify depending on operator
+stops_transit <- stops_transit |> 
+  mutate(
+    stop_category = case_when(
+      # BUS category
+      str_detect(stop_id, regex("carris|mobicascais|transportes_colectivos", ignore_case = TRUE)) ~ "BUS",
+      # Train & ferry category
+      str_detect(stop_id, regex("fertagus|comboios|ttsl|transtejo|soflusa", ignore_case = TRUE)) ~ "Train e ferry",
+      # Subway & light rail category
+      str_detect(stop_id, regex("metro|mts", ignore_case = TRUE)) ~ "Subway e light rail",
+      # Default fallback
+      TRUE ~ "Other"
+    )
+  )
+stops_transit <- stops_transit |> 
+  mutate(id = as.character(stop_index)) |> 
+  select(-stop_index, -stop_id, -stop_name)
+
+mapview(stops_transit, zcol = "stop_category")
+
+
+### Get isochrones for train/ferry stations using r5r (PRELIMINARY - NOT COMPLETE)
+
+# train and ferry - 15 min
 iso_train_ferry <- r5r::isochrone(
   r5r_network,
-  origins = train_ferry_coordinates,
-  mode = mode,
+  origins = stops_transit |> filter(stop_category == "Train e ferry"),
+  mode = "WALK",
+  cutoffs = 15,
   polygon_output = TRUE, 
-  cutoffs = time_interval,
-  departure_datetime = departure_datetime,
-  progress = TRUE,
-  zoom = 10
+  progress = TRUE
 )
+mapview(iso_train_ferry)
 
+iso_train_ferry_dissolved <- iso_train_ferry |> 
+  summarise(geometry = st_union(polygons)) |> 
+  st_make_valid()
+mapview(iso_train_ferry_dissolved)
+
+# subway and light rail - 10 min
+iso_train_subway <- r5r::isochrone(
+  r5r_network,
+  origins = stops_transit |> filter(stop_category == "Subway e light rail"),
+  mode = "WALK",
+  cutoffs = 10,
+  polygon_output = TRUE, 
+  progress = TRUE
+)
+mapview(iso_train_subway)
+
+iso_train_subway_dissolved <- iso_train_subway |> 
+  summarise(geometry = st_union(polygons)) |> 
+  st_make_valid()
+mapview(iso_train_subway_dissolved)
